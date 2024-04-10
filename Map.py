ebd48af88f681a8d705cd99565578ac5833f4f71
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-04-10 13:43:22 +0000

    Map should work for grey_scale

--- Map.py
+++ Map.py
@@ -3,27 +3,27 @@ Module used for creating and getting a matrixical map from an image
 """
 
 import numpy as np
+import math
 from skimage import io
 
 class Map(object):
-    def __init__(self, file_name:str, binary:bool = False, alpha:bool = False, mono:bool = False, binary_dark_value:any = 0, binary_light_value:any = 1):
+    def __init__(self, file_name:str, binary:bool = False, alpha:bool = False, grey_scale:bool = False, binary_dark_value:any = 0, binary_light_value:any = 1):
         """
         Creates a new map from an image
-        args: str file_name, bool binary, bool alpha, bool mono_color, any binary_dark_value, any binary_light_value
+        args: str file_name, bool binary, bool alpha, bool grey_scale, any binary_dark_value, any binary_light_value
         *args: str file_name
         return: None
         """
         
         im = io.imread(file_name) #RGBA format
         self.imarr = np.array(im)
-        
-        if mono:
-            alpha = False
-            self.imarr = (self.imarr[ :, :, 0] + self.imarr[ :, :, 1] + self.imarr[ :, :, 2]) / 3
 
         #remove alpha
         if not alpha:
             self.imarr = self.imarr[ :, :, 0:3] #RGB format
+            
+        if grey_scale: #mono removes alpha
+            self.imarr = math.floor((self.imarr[ :, :, 0] + self.imarr[ :, :, 1] + self.imarr[ :, :, 2]) / 3)
             
         if binary:
             self.imarr[self.imarr < 128] = binary_dark_value
